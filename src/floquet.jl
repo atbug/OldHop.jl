e9ae7a549eb84360f58441bf6cdd3b3f94538344
Chong Wang <chongwang1209@163.com>
2018-08-26 14:27:12 +0900

    fix Floquet conventions

--- src/floquet.jl
+++ src/floquet.jl
@@ -13,12 +13,13 @@ Calculate Floquet Hamiltonian. Generally, Floquet theory deals with time periodi
 Hamiltonians ``H(t+Ω)=H(t)``.
 
 `Hs` is a list of Hamiltonian looking like ``[H_0, H_1, H_2, ...]``.
-``H_i`` is coefficient of Fourier expansion of ``H(t)``:
-``H(t)=H_0+(H_1 e^{iΩt}+H_2 e^{2iΩt}+...+c.c.)``.
+``H_n`` is defined as:
+``H_n=\\frac{1}{T}\\int_0^T e^{inΩt} dt``.
+With basis functions defined as ``u_n=e^{-inΩt}``, ``H^F_{mn}=H_{m-n}-mΩδ_{mn}``.
 
 The basis of returned Floquet Hamiltonian is
-``(e^{-iNΩt}, e^{-i(N-1)Ωt}, ..., e^{iNΩt})``
-where internal degree of freedom is omitted. Therefore, ``H^F_{mn}=H_{n-m}-mΩδ_{mn}``
+``(e^{iNΩt}, e^{i(N-1)Ωt}, ..., e^{-iNΩt})``
+where internal degree of freedom is omitted.
 """
 function cal_floquet_hamiltonian(Hs::Vector{Matrix{T}}, Ω::Real, N::Integer) where T<:Number
     dim = size(Hs[1], 1)
@@ -28,10 +29,10 @@ function cal_floquet_hamiltonian(Hs::Vector{Matrix{T}}, Ω::Real, N::Integer) wh
     for j = 0:(nhams-1)
         for i = 1:(2N+1-j)
             if j == 0
-                HF[dim*(i-1)+1:dim*i, dim*(i-1)+1:dim*i] = Hs[1] + (i-N-1)*Ω*I
+                HF[dim*(i-1)+1:dim*i, dim*(i-1)+1:dim*i] = Hs[1]-(i-N-1)*Ω*I
             else
-                HF[dim*(i-1)+1:dim*i, dim*(i-1+j)+1:dim*(i+j)] = Hs[j+1]
-                HF[dim*(i-1+j)+1:dim*(i+j), dim*(i-1)+1:dim*i] = Hs[j+1]'
+                HF[dim*(i-1)+1:dim*i, dim*(i-1+j)+1:dim*(i+j)] = Hs[j+1]'
+                HF[dim*(i-1+j)+1:dim*(i+j), dim*(i-1)+1:dim*i] = Hs[j+1]
             end
         end
     end
@@ -48,7 +49,7 @@ cal_illuminated_hamiltonian(t::TightBindingModel, k::Vector{<:Real}; A::Vector{<
 Calculate Floquet Hamiltonian of `t` at `k` point under the illumination of light with frequency Ω.
 Peierls substitution is performed to the lowest order. Only electric field is taken
 into account. The parameter `A` looks like ``[A_x, A_y, A_z]`` denoting vector potential
-``A(t)=[A_x, A_y, A_z]e^{iΩt}+c.c.``. Floquet Hamiltonian is truncated
+``A(t)=[A_x, A_y, A_z]e^{-iΩt}+c.c.``. Floquet Hamiltonian is truncated
 up to `N` harmonics.
 """
 function  cal_illuminated_hamiltonian(t::TightBindingModel, k::Vector{<:Real}; A::Vector{<:Number},
@@ -86,7 +87,7 @@ Calculate bands of `t` under the illumination of light with frequency Ω.
 should be provided in reduced coordinates. Peierls substitution is performed to
 the lowest order. Only electric field is taken
 into account. The parameter `A` looks like ``[A_x, A_y, A_z]`` denoting vector potential
-``A(t)=[A_x, A_y, A_z]e^{iΩt}+c.c.``. Floquet Hamiltonian is truncated
+``A(t)=[A_x, A_y, A_z]e^{-iΩt}+c.c.``. Floquet Hamiltonian is truncated
 up to `N` harmonics.
 
 This function returns (`kdist`, `egvals`). `kdist` is the distance of k points and
--- test/floquet.jl
+++ test/floquet.jl
@@ -1,5 +1,6 @@
 let
-    HF = Hop.Floquet.cal_floquet_hamiltonian([ones(2, 2), 2*ones(2, 2)], 1, 2)
-    @test HF[1:2, 1:2] ≈ ones(2, 2) - 2*I
-    @test HF[3:4, 5:6] ≈ 2*ones(2, 2)
+    HF = Hop.Floquet.cal_floquet_hamiltonian([ones(2, 2), 2*ones(2, 2)*im], 1, 2)
+    @test HF[1:2, 1:2] ≈ ones(2, 2)+2*I
+    @test HF[5:6, 3:4] ≈ 2*ones(2, 2)*im
+    @test HF[3:4, 5:6] ≈ -2*ones(2, 2)*im
 end
